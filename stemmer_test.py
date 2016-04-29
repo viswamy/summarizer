9f10418e7a89448813a6dac754f4788b9ecb1d75
Virupaksha Swamy U M <vswamy@outlook.com>
2016-04-29 01:27:13 -0700

    changed function prototype

--- stemmer_test.py
+++ stemmer_test.py
@@ -4,11 +4,15 @@ import time
 import json
 import math
 
-def get_stem(self, word):
+model_file = open('prefix_suffix.json','r')
+model = json.loads(model_file.read())
+
+
+def get_stem(model, word):
     MAX_P = 0
     STEM = word
-    prefix_dictionary = self['prefix']
-    suffix_dictionary = self['suffix']
+    prefix_dictionary = model['prefix']
+    suffix_dictionary = model['suffix']
     for i in range(3, len(word)):
         prefix_str = word[0:i]
         suffix_str = word[i:len(word)]
@@ -25,10 +29,11 @@ def get_stem(self, word):
     return STEM
     
     
-model_file = open('prefix_suffix.json','r')
-model = json.loads(model_file.read())
-
+'''
 while(True):
     word = input('Enter the word here$$ ')
     print(get_stem(model, word))
+
+'''
+
 
